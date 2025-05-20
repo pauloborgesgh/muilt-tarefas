56334ebe8bd1a25380a51051efdf3c5a503771ca
borgesgh <pauloborgeszambon@gmail.com>
2025-05-19 21:25:55 -0300

    add_events

---
+++ src/app/components/eventos/emiter/emiter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emiter',
+  imports: [],
+  templateUrl: './emiter.component.html',
+  styleUrl: './emiter.component.css'
+})
+export class EmiterComponent {
+
+}
